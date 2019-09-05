f1eb032f13ec168d287292c0af9c2d081ba5afbf
Mirtles <mirte.post@yahoo.co.uk>
2019-09-05 16:31:32 +0200

    Add form styling.

--- src/components/CreateEventForm/CreateEventForm.js
+++ src/components/CreateEventForm/CreateEventForm.js
@@ -4,18 +4,18 @@ export default class EventForm extends React.Component {
   render() {
     return (<div>
       <h2>New event:</h2>
-      <form onSubmit={this.props.onSubmit}>
+      <form className="form" onSubmit={this.props.onSubmit}>
         <label>
           Event name:
-        <input type="text" value={this.props.values.name} onChange={this.props.onChange} name="name" />
+        <input className="input" type="text" value={this.props.values.name} onChange={this.props.onChange} name="name" />
         </label>
         <label>
           Event date:
-        <input type="text" value={this.props.values.date} onChange={this.props.onChange} name="date" />
+        <input className="input" type="text" value={this.props.values.date} onChange={this.props.onChange} name="date" />
         </label>
         <label>
           Event description:
-        <input type="text" value={this.props.values.description} onChange={this.props.onChange} name="description" />
+        <input className="input" type="text" value={this.props.values.description} onChange={this.props.onChange} name="description" />
         </label>
         <input type="submit" value="Submit" />
       </form>
